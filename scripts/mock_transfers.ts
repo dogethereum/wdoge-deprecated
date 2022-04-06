54558851ac63d24ae096982364745e276086e01c
Sebastián Claudio Nale <sebinale@gmail.com>
2022-04-06 11:15:03 -0300

    Adds a script that generates a few transactions with ERC20 events.

---
+++ scripts/mock_transfers.ts
@@ -0,0 +1,28 @@
+import hre from "hardhat";
+import { deployFixture } from "../deploy";
+
+async function initContractsLocal() {
+  const deployment = await deployFixture(hre);
+
+  // dogecoin has 8 decimals so 10^15 units = 10^7 dogetokens
+  const totalSupply = hre.ethers.BigNumber.from(10).pow(15);
+  const dogeToken = deployment.dogeToken.contract.connect(deployment.dogeToken.tokenAdmin);
+  await dogeToken.mint(totalSupply);
+
+  const accounts = await hre.ethers.getSigners();
+  const userA = accounts[3];
+  const userB = accounts[4];
+  const fiftyDoges = 5e9
+  await dogeToken.transfer(userA.address, fiftyDoges);
+  await dogeToken.transfer(userB.address, fiftyDoges);
+  await dogeToken.burn(fiftyDoges * 2);
+}
+
+initContractsLocal()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
