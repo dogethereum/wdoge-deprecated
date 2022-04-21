0cb4ee15c06fd5645a7a53025c59cecf153c9531
Sebastián Claudio Nale <sebinale@gmail.com>
2022-04-21 16:47:29 -0300

    Adds colours to inspect proxy task.

--- tasks/inspectProxy.ts
+++ tasks/inspectProxy.ts
@@ -1,3 +1,4 @@
+import chalk from "chalk";
 import fs from "fs-extra";
 import { task } from "hardhat/config";
 import type { ActionType } from "hardhat/types";
@@ -21,10 +22,10 @@ Ensure the correct network is passed to the --network parameter.`
   const { dogeToken } = await loadDeployment(hre, deploymentDir);
 
   console.log(`DogeToken proxy:
-  Token address is ${dogeToken.contract.address}.
-  Token administrator is ${dogeToken.tokenAdmin}.
-  Proxy administrator is ${dogeToken.proxyAdmin}.
-  The proxy currently forwards calls to implementation contract at address ${dogeToken.logicContractAddress}`);
+  Token address is ${chalk.green(dogeToken.contract.address)}.
+  Token administrator is ${chalk.green(dogeToken.tokenAdmin)}.
+  Proxy administrator is ${chalk.green(dogeToken.proxyAdmin)}.
+  The proxy currently forwards calls to implementation contract at address ${chalk.green(dogeToken.logicContractAddress)}`);
 };
 
 export const inspectProxyTaskName = generateTaskName("inspectProxy");
