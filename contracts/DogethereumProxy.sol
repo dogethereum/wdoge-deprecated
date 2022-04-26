04dbd1f194ff8e47e73c703d25a8949a45710e07
Sebastián Claudio Nale <sebinale@gmail.com>
2022-04-26 14:32:49 -0300

    Adds comments to the DogethereumProxy.

--- contracts/DogethereumProxy.sol
+++ contracts/DogethereumProxy.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 
+/**
+ * This contract allows us to work around a limitation of
+ * the hardhat-etherscan plugin to verify the proxy source code.
+ */
 contract DogethereumProxy is TransparentUpgradeableProxy {
 	constructor(address logic, address admin, bytes memory data) TransparentUpgradeableProxy(logic, admin, data) {}
 }
