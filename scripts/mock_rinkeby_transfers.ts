4bbfde9640f45238e88b73321706f506ede7ba81
Sebastián Claudio Nale <sebinale@gmail.com>
2022-05-17 16:27:14 -0300

    Renames in a few stray comments.

--- scripts/mock_rinkeby_transfers.ts
+++ scripts/mock_rinkeby_transfers.ts
@@ -4,12 +4,12 @@ import type { ContractTransaction } from "ethers";
 
 async function mockTransfers() {
   const deployment = await loadDeployment(hre);
-  const tokenAdmin = await hre.ethers.getSigner(deployment.wDoge.tokenOwner);
+  const tokenOwner = await hre.ethers.getSigner(deployment.wDoge.tokenOwner);
   const userA = await hre.ethers.getSigner("0x1f8826b2A56e84058455d2687c46Fc44C9F43F4D");
 
   // dogecoin has 8 decimals so 10^20 units = 10^12 WDoges
   const workingSupply = hre.ethers.BigNumber.from(10).pow(20);
-  const wDoge = deployment.wDoge.contract.connect(tokenAdmin);
+  const wDoge = deployment.wDoge.contract.connect(tokenOwner);
 
   const dummyTxId = `0x${"0".repeat(64)}`;
   let tx: ContractTransaction = await wDoge.mint(workingSupply, dummyTxId);
@@ -25,7 +25,7 @@ async function mockTransfers() {
     await wDoge.transfer(userA.address, suppliedAmount, { gasLimit: 80_000 });
     await wDoge
       .connect(userA)
-      .transfer(tokenAdmin.address, suppliedAmount.div(10), { gasLimit: 80_000 });
+      .transfer(tokenOwner.address, suppliedAmount.div(10), { gasLimit: 80_000 });
   }
   const fiftyDoges = 5e9;
   tx = await wDoge.burn(fiftyDoges, { gasLimit: 80_000 });
